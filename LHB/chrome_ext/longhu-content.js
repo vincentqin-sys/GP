85d5b7a45b85a145583da2c4e33391d630fb7181
yioo_2007 <yioo_2007@126.com>
2022-09-26 12:32:36 +0800

    ok

--- LHB/chrome_ext/longhu-content.js
+++ LHB/chrome_ext/longhu-content.js
@@ -2,6 +2,14 @@
 temp = document.createElement('script');
 temp.setAttribute('type','text/javascript');
 temp.setAttribute('charset', "UTF-8");
-temp.src = chrome.extension.getURL('longhu.js');
+temp.src = chrome.extension.getURL('longhu-name-parse.js');
+temp.async = false;
+document.documentElement.appendChild(temp);
+
+
+temp = document.createElement('script');
+temp.setAttribute('type','text/javascript');
+temp.setAttribute('charset', "UTF-8");
+temp.src = chrome.extension.getURL('longhu-data-parse.js');
 temp.async = false;
 document.documentElement.appendChild(temp);
