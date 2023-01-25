c044b4cdadcabc07808855c802d7dcc5f70eeecc
yioo_2007 <yioo_2007@126.com>
2023-01-25 22:26:54 +0800

    d

---
+++ HGT/fetch_hgt.py
@@ -0,0 +1,268 @@
+import sqlite3
+from time import sleep
+from selenium import webdriver
+import traceback
+
+# chrome.exe --remote-debugging-port=9998 --user-data-dir="D://download/chrome"
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# options.add_experimental_option("debuggerAddress", "127.0.0.1:9998")
+# options.add_argument("--user-data-dir=C:\\Users\\ROG\\AppData\\Local\\Google\\Chrome\\User Data")
+
+# options.add_argument('-headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36')
+
+browser = webdriver.Chrome( options = options)
+# 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+
+"""
+with open('stealth.min.js') as f:
+    js = f.read()
+    browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": js
+    })
+"""
+
+db = sqlite3.connect('hgt.db')
+
+# 加载指数数据 北向资金流入
+def load_zs_data(lastDay) :
+    url = 'https://data.eastmoney.com/hsgtcg/gzcglist.html'
+    browser.get(url)
+    sleep(3) # 2 second
+    # browser.find_element_by_id('page-50').click()
+    # sleep(1)
+    table = browser.find_element_by_css_selector('.dataview-body > table')
+    headElems = table.find_elements_by_xpath('.//thead/tr/th')
+    if headElems[0].text != '日期' or  headElems[2].text != '北向资金今日增持估计' or  headElems[6].text != '市值':
+        print('ZS Web page changed, update program first... ', url)
+        raise Exception('Error: page changed ')
+        return False
+    data = []
+    while True:
+        bodyElems = table.find_elements_by_xpath('.//tbody/tr')
+        # print('bodyElems.len = ', len(bodyElems))
+        for e in bodyElems:
+            tds = e.find_elements_by_xpath('.//td')
+            item = {'code': 'HGTALL'}
+            item['day'] = tds[0].text
+            item['jme'] = tds[2].text
+            if item['day'] > lastDay:
+                data.append(item)
+            else:
+                break
+        #table.find_element_by_link_text('下一页').click()
+        #sleep(3)
+        #print('Wait for click 下一页')
+        break
+    return data
+
+
+def load_top10_body(table, data, lastDay):
+    bodyElems = table.find_elements_by_xpath('.//tbody/tr')
+    for e in bodyElems:
+        tds = e.find_elements_by_xpath('.//td')
+        item = {}
+        item['day'] = tds[0].text
+        item['code'] = tds[1].find_element_by_xpath('.//a').text
+        item['name'] = tds[2].find_element_by_xpath('.//a').text
+
+        item['mrje'] = tds[5].find_element_by_xpath('.//i').text
+        item['mrzb'] = tds[6].text
+
+        item['mcje'] = tds[7].find_element_by_xpath('.//i').text
+        item['mczb'] = tds[8].text
+
+        item['cjje'] = tds[9].find_element_by_xpath('.//i').text
+        item['cjzb'] = tds[10].text
+
+        if item['day'] <= lastDay:
+            return False
+        data.append(item)
+    return True
+
+def load_top10_one_table(browser, divId, day):
+    div = browser.find_element_by_id(divId)
+    table = div.find_element_by_class_name('dataview-body').find_element_by_xpath('.//table')
+    headElems = table.find_elements_by_xpath('.//thead/tr/th')
+    if headElems[1].get_attribute('data-field') != 'SECURITY_CODE' or \
+                headElems[2].get_attribute('data-field') != 'SECURITY_NAME' or  \
+                headElems[6].get_attribute('data-field') != 'NET_BUY_AMT' or  headElems[7].get_attribute('data-field') != 'BUY_AMT' or \
+                headElems[8].get_attribute('data-field') != 'SELL_AMT' or  headElems[9].get_attribute('data-field') != 'DEAL_AMT'  :
+        print('Web page changed, update program first...')
+        return False
+    data = []
+    bodyElems = table.find_elements_by_xpath('.//tbody/tr')
+    for e in bodyElems:
+        tds = e.find_elements_by_xpath('.//td')
+        if len(tds) == 1:
+            # no data
+            print('No Data')
+            break
+        item = {}
+        item['day'] = day
+        item['code'] = tds[1].find_element_by_xpath('.//a').text
+        item['name'] = tds[2].find_element_by_xpath('.//a').text
+        item['jme'] = tds[6].find_element_by_xpath('.//span').text
+        item['mrje'] = tds[7].text
+        item['mcje'] = tds[8].text
+        item['cjje'] = tds[9].text
+        data.append(item)
+    return data
+
+
+# 十大成交股
+def load_top10_data(day) :
+    day = str(day)
+    day = day[0:4] + '-' + day[4:6] + '-' + day[6:]
+    url = 'http://data.eastmoney.com/hsgt/top10/{}.html'.format(day)
+    browser.get(url)
+    sleep(3) # 3 second
+    
+    data = []
+    hgt = load_top10_one_table(browser, 'dataview_hgt', day)
+    sgt = load_top10_one_table(browser, 'dataview_sgt', day)
+    
+    if hgt == False or sgt == False:
+        return False
+    data.extend(hgt)
+    data.extend(sgt)
+    return data
+
+def formatMoney(text, info):
+    if type(text) != str:
+        return text
+    lastChar = text[-1:]
+    money = float(text[0: -1])
+
+    if lastChar == '亿':
+        money = money * 10000
+    elif lastChar != '万':
+        print(info)
+        input('Format money error: ' + text)
+    return int(money)
+
+def formatDBData(p):
+    for item in p:
+        item['mrje'] = formatMoney(item.get('mrje', 0), item)
+        item['mcje'] = formatMoney(item.get('mcje', 0), item)
+        if 'cjje' in item:
+            item['cjje'] = formatMoney(item['cjje'], item)
+        else:
+            item['cjje'] = item['mrje'] + item['mcje']
+        if 'jme' not in item:
+            item['jme'] = item['mrje'] - item['mcje']
+        else:
+            item['jme'] = formatMoney(item.get('jme', 0), item)
+        item['day'] = item['day'].replace('-', '')
+        if 'mrzb' in item:
+            item['mrzb'] = int(float(item['mrzb'][0:-1]))
+        if 'mczb' in item:
+            item['mczb'] = int(float(item['mczb'][0:-1]))
+
+        if item['code'] == 'HGTALL': #单位亿元
+            item['jme'] = item['jme'] // 10000
+            item['mrje'] = item['mrje'] // 10000
+            item['mcje'] = item['mcje'] // 10000
+            item['cjje'] = item['cjje'] // 10000
+
+
+def saveDB(data):
+    try:
+        cursor = db.cursor()
+
+        formatDBData(data)
+        for v in data:
+            sql = 'insert into hgt (code, day, jme, mrje, mcje, cjje) values ("{0}", {1}, {2}, {3}, {4}, {5}) '.format(v['code'], v['day'], v['jme'], (v['mrje']), (v['mcje']), v['cjje'])
+            print(sql)
+            cursor.execute(sql)
+        db.commit()
+        return True
+    except Exception as e:
+        print('DB save error: ', str(e))
+        traceback.print_exc()
+        db.rollback()
+        return False
+
+def queryLastDay():
+    cursor = db.cursor()
+    sql = 'select max(day) from hgt where code = "HGTALL" '
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    if not res[0]:
+        zsLastDay = '20221010'
+    else:
+        zsLastDay = str(res[0])
+    zsLastDay = zsLastDay[0:4] + '-' + zsLastDay[4:6] + '-' + zsLastDay[6:]
+    print('指数 last day: ', zsLastDay)
+
+    sql = 'select max(day) from hgt where  code != "HGTALL" '
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    lastDay = str(res[0])
+    lastDay = lastDay[0:4] + '-' + lastDay[4:6] + '-' + lastDay[6:]
+    print('GP last day: ', lastDay)
+    
+    return [zsLastDay, lastDay]
+    #db.commit()
+    print('DB query error: ', str(e))
+    
+
+def queryCodeDays(lastDay):
+    lastDay = lastDay[0:4] + lastDay[5:7] + lastDay[8:]
+    cursor = db.cursor()
+    sql = 'select day from hgt  where code = "HGTALL" and day > {} order by day asc'.format(lastDay)
+    print(sql)
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    data = []
+    for r in res:
+        data.append(r[0])
+    return data
+
+def main(auto):
+    lastDays = queryLastDay()
+    if lastDays == False:
+        return
+
+    dataZS = load_zs_data(lastDays[0])
+    if dataZS == False :
+        print('Load ZS fail')
+        return
+    print('ZS --> ', len(dataZS))
+    for i in dataZS:
+        print('   ', i)
+
+    if saveDB(dataZS) == False:
+        return
+    
+    days = queryCodeDays(lastDays[1])
+    if days == False:
+        return
+    
+    for day in days:
+        dataHGT = load_top10_data(day)
+        if not dataHGT:
+            continue
+        print('HGT --> ', len(dataHGT))
+        for i in dataHGT:
+            print('   ', i)
+        saveDB(dataHGT)
+    db.close()
+    
+    if (not auto) and (len(days) == 0):
+        ld = str(lastDays[2])
+        #ld = ld[0:4] + '-' + ld[4:6] + '-' + ld[6:]
+        url = 'http://data.eastmoney.com/hsgt/top10/{}.html'.format(ld)
+        browser.get(url)
+    if not auto:
+        input('Press Enter Key To Exit')
+    
+    browser.quit()
+
+if __name__ == '__main__':
+    main(True)
+    
+
---
+++ HGT/fetch_hgt_cgsl.py
@@ -0,0 +1,146 @@
+import pymysql
+import time
+from time import sleep
+from selenium import webdriver
+import traceback
+
+# chrome.exe --remote-debugging-port=9998 --user-data-dir="D://download/chrome"
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# options.add_experimental_option("debuggerAddress", "127.0.0.1:9998")
+# options.add_argument("--user-data-dir=C:\\Users\\ROG\\AppData\\Local\\Google\\Chrome\\User Data")
+
+# options.add_argument('-headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36')
+
+browser = webdriver.Chrome( options = options)
+browser.implicitly_wait(10)
+
+mysql_conn = pymysql.connect(host= '127.0.0.1', port= 3306, user= 'root', password= 'root', db= 'tdx_f10')
+
+needLoadCodes = []
+loadIdx = 0
+
+def load_code_cgsl(code):
+    url = 'http://data.eastmoney.com/hsgtcg/StockHdStatistics.aspx?stock={}'.format(code)
+    browser.get(url)
+    div = browser.find_element_by_class_name('dataview-body')
+    table = div.find_element_by_tag_name('table')
+    trHead = table.find_elements_by_xpath('.//thead/tr/th')
+    # check head column
+    if trHead[0].get_attribute('data-field') != 'TRADE_DATE' or \
+        trHead[2].get_attribute('data-field') != 'CLOSE_PRICE' or \
+        trHead[4].get_attribute('data-field') != 'HOLD_SHARES' or \
+        trHead[6].get_attribute('data-field') != 'A_SHARES_RATIO' :
+            print('Web page change, update program first...')
+            return False
+
+    data = []
+    trData = table.find_elements_by_xpath('.//tbody/tr')
+    if len(trData) < 2:
+        # no data
+        return data;
+    for tr in trData:
+        tds = tr.find_elements_by_xpath('.//td')
+        day = tds[0].text
+        price = tds[2].find_element_by_xpath('.//span').text
+        cgsl = tds[4].text
+        per = tds[6].text
+        data.append({'day': day, 'cgsl': cgsl, 'per': per, 'price': price})
+    return data
+
+def formatCgsl(text):
+    lastChar = text[-1:]
+    sl = float(text[0: -1])
+
+    if lastChar == '亿':
+        sl = sl * 10000
+    elif lastChar == '万':
+        sl = sl
+    else:
+        input('Format cgsl error: ' + text)
+    return int(sl)
+
+def save_cgsl(code, data):
+    cursor = mysql_conn.cursor()
+    n1 = 0
+    n2 = 0
+    n3 = 0
+    for r in data:
+        day = r['day'].replace('-', '')
+        sql = 'select _id, _cgsl, _per, _zsz from _hgt_acc where _code = "{}" and _day = {}'.format(code, day)
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        
+        cgsl = formatCgsl(r['cgsl'])
+        per = float(r['per'])
+        zsz = int(cgsl * 100 / per * float(r['price']) / 10000) # 亿元
+        if len(res) == 0:
+            sql = 'insert into _hgt_acc (_day, _code, _cgsl, _per, _zsz) values({}, "{}", {}, {}, {})'.format(day, code, cgsl, per, zsz)
+            n1 += 1
+        else:
+            res = res[0]
+            if res[1] != 0 and res[2] != 0:
+                n3 += 1
+                continue
+            zsz = zsz if res[3] == 0 else res[3]
+            sql = 'update _hgt_acc set _cgsl = {} , _per = {}, _zsz = {} where _id = {}'.format(cgsl, per, zsz, res[0])
+            n2 += 1
+        cursor.execute(sql)
+    mysql_conn.commit()
+    print('[{}] {}  insert:{}   update:{}  exists:{}'.format(loadIdx, code, n1, n2, n3) )
+
+def load_one_code(code):
+    try:
+        data = load_code_cgsl(code)
+        if data != False:
+            if save_cgsl(code, data) != False:
+                # load success
+                return True
+    except Exception as e:
+        print('Load {} error: '.format(code), str(e))
+        traceback.print_exc()
+    # load fail
+    return False
+
+def load_all_codes():
+    global loadIdx
+    print('Total : {}'.format(len(needLoadCodes)))
+    while len(needLoadCodes) > 0:
+        target = needLoadCodes[0]
+        ok = load_one_code(target)
+        needLoadCodes.remove(target)
+        if not ok:
+            needLoadCodes.append(target)
+        else:
+            loadIdx += 1
+        sleep(3)
+    
+
+
+def main():
+    sql = 'select  _code, max(_zsz) from _hgt_acc group by _code'
+    cursor = mysql_conn.cursor()
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    # res = [['002008', 500]]
+    for r in res:
+        code = r[0]
+        zsz = r[1]
+        #if zsz > 350 or zsz == 0: #总市值大于350亿
+        needLoadCodes.append(code)
+
+    st = time.time()
+    load_all_codes()
+    ut = int(time.time() - st) # second
+    mysql_conn.close()
+    browser.quit()
+    # print('Use Time: {}h  {}m  {}s'.format(ut // 3600, ut // 60, ut % 60))
+    # input('Press Enter Key To Exit')
+
+main()
+
+#  查找沪股通占股比例最小、最大值，并排序
+#  select a.*, a._max_per - a._min_per as _diff from hgt_acc_per_view a order by _diff desc;
