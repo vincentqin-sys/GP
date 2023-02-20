0c0c4a60d187fc05268513a2c3777d7253923d48
yioo_2007 <yioo_2007@126.com>
2023-02-20 15:37:04 +0800

    update db

---
+++ start-all.bat
@@ -0,0 +1,14 @@
+echo "启动LHB Server"
+start "" "LHB/server/startup.py"
+
+echo "启动JGD Server"
+start "" "JGD/startup.py"
+
+echo "启动HGT Server"
+start "" "HGT/startup.py"
+
+echo "启动THS Server"
+start "" "THS/startup.py"
+
+echo "启动THS Query"
+start "" "THS/query.py"
