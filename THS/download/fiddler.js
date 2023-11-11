e64ff72c0edb4a360b7355564b1faec74cb4c58e
gaoyan <yioo_2007@126.com>
2023-11-11 22:49:52 +0800

    ths

--- THS/download/fiddler.js
+++ THS/download/fiddler.js
@@ -1,5 +1,8 @@
    // 修改fiddler的rule
 
+   import System.IO;
+   import System.Text;
+
     static function getParams(url) {
         var rt = new Object();
         var idx = url.IndexOf('?');
@@ -13,6 +16,18 @@
             rt[kv[0]] = kv[1];
         }
         return rt;
+    }
+
+    // 是否是交易时间段
+    static function isTradeTime() {
+        var dw = DateTime.Now.DayOfWeek;
+        if (dw >= 1 && dw <= 5) { // 周一至周五
+            var tt = DateTime.Now.ToString('t') // 13:09	HH:MM
+            if (tt >= '09:00' && tt < '15:00') {
+                return true;
+            }
+        }
+        return false;
     }
 
     static function OnBeforeResponse(oSession: Session) {
@@ -39,7 +54,7 @@
         
         // 同花顺Level-2功能： 大单结构(主动买入大单，被动买入大单，主动卖出大单，被动卖出大单)
         // stockCode=601127&start=20231107&dayNum=0
-        if (furl.StartsWith("http://apigate.10jqka.com.cn/d/charge/orderprism/orderStruct?")) {
+        if (!isTradeTime() && furl.StartsWith("http://apigate.10jqka.com.cn/d/charge/orderprism/orderStruct?")) {
             var params = getParams(furl);
             var code = params['stockCode'];
             var startDay = params['start'];
@@ -51,14 +66,31 @@
 				if (body.Length < 20) {
 					return;
 				}
-				var ts = DateTime.Now.ToString();
+				var ts = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
                 body = ts + '\t' + code + '\t' + startDay + '\t' + dayNum + '\n' + body + '\n';
-                var path = "D:/ths/struct/" + code;
+                var path = "D:/ths/ddlr-struct/" + code;
                 var fs = new FileStream(path, FileMode.Append, FileAccess.Write);
                 var bb = new UTF8Encoding(true).GetBytes(body);
                 fs.Write(bb, 0, bb.Length);
                 fs.Close();
+                return;
             }
         }
-        
+
+        // 同花顺Level-2功能：大单买卖详细数据
+        // stockCode=601136
+        if (!isTradeTime() && furl.StartsWith("http://apigate.10jqka.com.cn/d/charge/orderprism/historyOrder?")) {
+            var params = getParams(furl);
+            var code = params['stockCode'];
+            oSession.utilDecodeResponse();
+            var body = oSession.GetResponseBodyAsString();
+            var ts = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var path = "D:/ths/ddlr-detail/" + code;
+            body = ts + '\t' + code + '\n' + body + '\n';
+            var fs = new FileStream(path, FileMode.Append, FileAccess.Write);
+            var bb = new UTF8Encoding(true).GetBytes(body);
+            fs.Write(bb, 0, bb.Length);
+            fs.Close();
+            return;
+        }
     }
--- THS/download/load_ths_ddlr.py
+++ THS/download/load_ths_ddlr.py
@@ -1,14 +1,18 @@
 import peewee as pw
 from peewee import fn
 import os, json, time, sys, pyautogui
+import io
 
 sys.path.append('.')
 import orm
 
-BASE_DATA_PATH = 'D:/ths/struct/'
+BASE_STRUCT_PATH = 'D:/ths/ddlr-struct/'
+BASE_DETAIL_PATH = 'D:/ths/ddlr-detail/'
 
-if not os.path.exists(BASE_DATA_PATH):
-    os.makedirs(BASE_DATA_PATH)
+if not os.path.exists(BASE_STRUCT_PATH):
+    os.makedirs(BASE_STRUCT_PATH)
+if not os.path.exists(BASE_DETAIL_PATH):
+    os.makedirs(BASE_DETAIL_PATH)
 
 def isCode(name):
     if not name:
@@ -20,75 +24,140 @@ def isCode(name):
             return False
     return True
 
-def listFiles():
-    fs = os.listdir(BASE_DATA_PATH)
-    rs = [BASE_DATA_PATH + f for f in fs if isCode(f) ]
-    return rs
-
-def loadFileData(fileName):
-    data = {}
-    f = open(fileName, 'r', encoding= 'utf8')
-    lines = f.readlines()
-    f.close()
-    i = 0
-    while i < len(lines) - 1:
-        heads = lines[i].strip().split('\t')
-        if len(heads) != 4 or len(lines[i + 1]) < 10:
-            i += 1
-            continue
-        curTime, code, day, n = heads
-        curDay, curTime = curTime.split(' ')
-        curDay = curDay.replace('-', '')
-        curTime = curTime[0 : 5] # hh:mm
-        if curDay == day:
-            if curTime < '15:00':
-                i += 2
-                continue
-            
-        js = json.loads(lines[i + 1])
-        if js['code'] != 0:
-            continue
-        row = {'code' : code, 'day' : day}
-        keys = ['activeIn', 'activeOut', 'positiveIn', 'positiveOut']
-        for k in keys:
-            row[k] = js['data'][k] / 100000000 # 亿
-        row['total'] = row['activeIn'] + row['positiveIn'] - row['activeOut'] - row['positiveOut']
-        data[day] = row
-        i += 2
-    
-    # merge data
-    rt = []
-    for k, v in data.items():
-        rt.append(v)
-    return rt
-
-def loadAllFileData():
-    fs = listFiles()
-    for f in fs:
-        data = loadFileData(f)
-        if len(data) <= 0:
-            continue
-        mergeSavedData(data)
-        os.remove(f)
-
-def getNameByCode(code):
+def getNameByCode(self, code):
     n = orm.THS_Newest.get_or_none(orm.THS_Newest.code == code)
     if not n:
         return ''
     return n.name
 
-def mergeSavedData(datas):
-    code = datas[0]['code']
-    name = getNameByCode(code)
-    maxDay = orm.THS_DDLR.select(pw.fn.Max(orm.THS_DDLR.day)).where(orm.THS_DDLR.code == code).scalar()
-    if not maxDay:
-        maxDay = ''
-    for d in datas:
-        if d['day'] > maxDay:
-            d['name'] = name
-            orm.THS_DDLR.create(**d)
+class LoadThsDdlrStruct:
+    def listFiles(self):
+        fs = os.listdir(BASE_STRUCT_PATH)
+        rs = [BASE_STRUCT_PATH + f for f in fs if isCode(f) ]
+        return rs
 
-def autoLoadTop200Data():
+    def loadFileData(self, fileName):
+        data = {}
+        f = open(fileName, 'r', encoding= 'utf8')
+        lines = f.readlines()
+        f.close()
+        i = 0
+        while i < len(lines) - 1:
+            heads = lines[i].strip().split('\t')
+            if len(heads) != 4 or len(lines[i + 1]) < 10:
+                i += 1
+                continue
+            curTime, code, day, n = heads
+            curDay, curTime = curTime.split(' ')
+            curDay = curDay.replace('-', '')
+            curTime = curTime[0 : 5] # hh:mm
+            if curDay == day:
+                if curTime < '15:00':
+                    i += 2
+                    continue
+                
+            js = json.loads(lines[i + 1])
+            if js['code'] != 0:
+                continue
+            row = {'code' : code, 'day' : day}
+            keys = ['activeIn', 'activeOut', 'positiveIn', 'positiveOut']
+            for k in keys:
+                row[k] = js['data'][k] / 100000000 # 亿
+            row['total'] = row['activeIn'] + row['positiveIn'] - row['activeOut'] - row['positiveOut']
+            data[day] = row
+            i += 2
+        
+        # merge data
+        rt = []
+        for k, v in data.items():
+            rt.append(v)
+        return rt
+
+    def loadAllFileData(self):
+        fs = self.listFiles()
+        for f in fs:
+            data = self.loadFileData(f)
+            if len(data) <= 0:
+                continue
+            self.mergeSavedData(data)
+            os.remove(f)
+
+    def mergeSavedData(self, datas):
+        code = datas[0]['code']
+        name = self.getNameByCode(code)
+        maxDay = orm.THS_DDLR.select(pw.fn.Max(orm.THS_DDLR.day)).where(orm.THS_DDLR.code == code).scalar()
+        if not maxDay:
+            maxDay = ''
+        for d in datas:
+            if d['day'] > maxDay:
+                d['name'] = name
+                orm.THS_DDLR.create(**d)
+
+class LoadThsDdlrDetail:
+    def __init__(self) -> None:
+        self.tradeDays = self.getMaxTradeDays()
+        print(self.tradeDays)
+        pass
+
+    def getMaxTradeDays(self):
+        query = orm.THS_Hot.select(orm.THS_Hot.day).distinct().order_by(orm.THS_Hot.day.desc()).limit(10).tuples()
+        #print(query)
+        maxDays = [str(d[0]) for d in query]
+        return maxDays
+
+    def getTradeDay(self, writeDay):
+        for d in self.tradeDays:
+            if writeDay >= d:
+                return d
+        raise Exception('出错了')
+
+    def loadAllFilesData(self):
+        fs = os.listdir(BASE_DETAIL_PATH)
+        for f in fs:
+            if isCode(f):
+                fp = BASE_DETAIL_PATH + f
+                self.loadFileData(fp)
+                os.remove(fp)
+    
+    # 写入 xxxxxx.dd 文件， 数据格式： 日期;开始时间,买卖方式(1:主动买 2:被动买 3:主动卖 4:被动卖),成交金额(万元); ...
+    def loadFileData(self, fileName):
+        f = open(fileName, 'r', encoding= 'utf8')
+        lines = f.readlines()
+        f.close()
+        i = 0
+        sio = io.StringIO()
+        while i < len(lines) - 1:
+            heads = lines[i].strip().split('\t')
+            if len(heads) != 2 or len(lines[i + 1]) < 10:
+                i += 1
+                continue
+            curTime, code = heads
+            curDay, curTime = curTime.split(' ')
+            curDay = curDay.replace('-', '')
+            curTime = curTime[0 : 5] # hh:mm
+            tradeDay = self.getTradeDay(curDay)
+            ld = json.loads(lines[i + 1])
+            if ld['code'] != 0:
+                i += 2
+                continue
+            sio.write(tradeDay + ';')
+            for d in ld['data']:
+                v = d['firstTime'][0 : 4] + ',' + str(d['stats']) + ',' + str(int(d['totalMoney'] / 10000 + 0.5)) + ';'
+                sio.write(v)
+            sio.write('\n')
+            i += 2
+
+        f2 = open(fileName + '.dd', 'a', encoding='utf8')
+        f2.write(sio.getvalue())
+        f2.close()
+
+
+# 自动下载同花顺热点Top200个股的大单数据
+def autoLoadTop200Data(self):
+    print('自动下载Top 200大单买卖数据(同花顺Level-2)')
+    print('必须打开Fiddler, Fiddler拦截onBeforeResponse, 将数据下载下来')
+    print('再将同花顺的大单统计功能打开, 鼠标定位在输入框中')
+
     time.sleep(10)
     datas = orm.THS_Hot.select().order_by(orm.THS_Hot.id.desc()).limit(200)
     datas = [d for d in datas]
@@ -104,12 +173,6 @@ def autoLoadTop200Data():
         pyautogui.press('enter')
         time.sleep(5)
 
-def test():
-    query = orm.THS_DDLR.select()
-    for it in query:
-        name = getNameByCode(it.code)
-        it.name = name
-        it.save()
 
 def test2():
     import win32gui, win32con
@@ -125,10 +188,12 @@ def test2():
     win32gui.EnumChildWindows(MAIN_WIN, enumCallback, 'WWX')
 
 if __name__ == '__main__':
-    print('自动下载Top 200大单买卖数据(同花顺Level-2)')
-    print('必须打开Fiddler, Fiddler拦截onBeforeResponse, 将数据下载下来')
-    print('再将同花顺的大单统计功能打开, 鼠标定位在输入框中')
     autoLoadTop200Data()
-    loadAllFileData()
-    # test2()
-    pass
+
+    lds = LoadThsDdlrStruct()
+    lds.loadAllFileData()
+
+    ldd = LoadThsDdlrDetail()
+    # 写入 xxxxxx.dd 文件， 数据格式： 日期;开始时间,买卖方式(1:主动买 2:被动买 3:主动卖 4:被动卖),成交金额(万元); ...
+    ldd.loadAllFilesData()
+    
