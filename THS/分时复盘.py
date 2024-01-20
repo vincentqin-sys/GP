c514d73fff159d83f13e8e777533832f90728ea3
gaoyan <yioo_2007@126.com>
2024-01-20 23:29:27 +0800

    fp

--- THS/分时复盘.py
+++ THS/分时复盘.py
@@ -2,7 +2,6 @@ import win32gui, win32con , win32api, win32ui # pip install pywin32
 import threading, time, datetime, sys, os, copy
 import os, sys, requests
 import win32gui, win32con
-import orm, base_win, ths_win
 
 cwd = os.getcwd()
 w = cwd.index('GP')
@@ -10,6 +9,7 @@ cwd = cwd[0 : w + 2]
 sys.path.append(cwd)
 from Tdx import datafile
 from THS.download import henxin, load_ths_ddlr
+from THS import orm, base_win, ths_win
 
 class FenShiModel:
     def __init__(self):
@@ -81,6 +81,7 @@ class FenShiModel:
                     rd.append(d)
             if len(rd) > 0:
                 self.ddlrFilterData.append(rd)
+        return self.ddlrFilterData
 
     def getPriceRange(self):
         if not self.fsData:
@@ -345,16 +346,18 @@ class FenShiWindow(base_win.BaseWindow):
             return True
         return super().winProc(hwnd, msg, wParam, lParam)
 
-
 class TableWindow(base_win.BaseWindow):
     def __init__(self) -> None:
         super().__init__()
         self.data = None
+        self.totalData = None
         self.SCROLL_BAR_WIDTH = 10
         self.HEAD_HEIGHT = 35
+        self.TAIL_HEIGHT = 100
         self.ROW_HEIGHT = 30
         self.startIdx = 0
         self.selIdx = -1
+        self.filterName = None
 
     def drawHeadColumn(self, hdc):
         sz = self.getClientSize()[0] - self.SCROLL_BAR_WIDTH
@@ -365,9 +368,42 @@ class TableWindow(base_win.BaseWindow):
             rc = (i * cw, 10, (i + 1) * cw, self.HEAD_HEIGHT)
             self.drawer.drawText(hdc, t, rc, 0xffffff)
 
+    def drawTailColumn(self, hdc):
+        def fmtMoney(m):
+            if abs(m) < 10000:
+                return f'{m}万'
+            return f'{m / 10000 :.02f}亿'
+
+        if not self.totalData:
+            return
+        sz = self.getClientSize()
+        ch = self.TAIL_HEIGHT // (len(self.totalData) + 1)
+        cw = sz[0] // 3
+        sy = sz[1] - self.TAIL_HEIGHT
+        self.drawer.fillRect(hdc, (0, sy, sz[0], sy + ch), 0x1A1A1A)
+        self.drawer.drawRect(hdc, (0, sy, sz[0], sy + ch), 0xcacaca)
+        titles = (str(self.filterName), '个数', '总金额') # , '主动', '被动'
+        for i, t in enumerate(titles):
+            rc = (i * cw, sy + 5, (i + 1) * cw, sy + ch)
+            self.drawer.drawText(hdc, t, rc, 0xffffff)
+        sy += ch
+        for i, d in enumerate(self.totalData):
+            if d['name'] == '买单':
+                color = 0x2222ff
+            elif d['name'] == '卖单':
+                color = 0x22ff22
+            else:
+                color = 0x2222ff if d['money'] >= 0 else 0x22ff22
+            vals = (d['name'], d['num'], fmtMoney(d['money']))
+            for j in range(len(vals)):
+                rc = (j * cw, sy + 5, (j + 1) * cw, sy + ch)
+                self.drawer.drawText(hdc, str(vals[j]), rc, color)
+            sy += ch
+
+
     def getVisibleRange(self):
         rowNum = self.getMaxRowNum()
-        return (self.startIdx, min(self.startIdx + rowNum, len(self.data)))
+        return (self.startIdx, min(self.startIdx + rowNum, len(self.data) - 1))
     
     def drawRowItem(self, hdc, sy, data, cw):
         _time, bs, money, vol = data
@@ -385,13 +421,13 @@ class TableWindow(base_win.BaseWindow):
         self.drawer.drawText(hdc, f'{money}万', rc, color=color, align=win32con.DT_RIGHT)
 
     def drawRows(self, hdc):
-        sz = self.getClientSize()[0] - self.SCROLL_BAR_WIDTH
-        cw = sz // 3
+        w = self.getClientSize()[0] - self.SCROLL_BAR_WIDTH
+        cw = w // 3
         vr = self.getVisibleRange()
         y = self.HEAD_HEIGHT
         for i in range(*vr):
             if i == self.selIdx:
-                self.drawer.fillRect(hdc, (0, y, sz, y + self.ROW_HEIGHT), 0x393533)
+                self.drawer.fillRect(hdc, (0, y, w, y + self.ROW_HEIGHT), 0x393533)
             self.drawRowItem(hdc, y, self.data[i], cw)
             y += self.ROW_HEIGHT
     
@@ -417,10 +453,13 @@ class TableWindow(base_win.BaseWindow):
         if not self.data:
             return
         self.drawRows(hdc)
+        self.drawTailColumn(hdc)
 
-    def setData(self, data):
+    def setData(self, data, filterName):
+        self.filterName = filterName
         self.startIdx = 0
         self.selIdx = -1
+        self.totalData = None
         if not data:
             self.data = None
             return
@@ -428,21 +467,32 @@ class TableWindow(base_win.BaseWindow):
         for ds in data:
             for d in ds:
                 self.data.append(d)
+        buy = {'name': '买单', 'num' : 0, 'money': 0, 'zdMoney': 0, 'bdMoney': 0}
+        sell = {'name': '卖单','num' : 0, 'money': 0, 'zdMoney': 0, 'bdMoney': 0}
+        for d in self.data:
+            _t, bs, money, *_ = d
+            rr = buy if bs <= 2 else sell
+            rr['num'] += 1
+            rr['money'] += money
+            if bs % 2 == 1:
+                rr['zdMoney'] += money
+            else:
+                rr['bdMoney'] += money
+        total = {'name':'差值', 'num': buy['num'] - sell['num'], 'money': buy['money'] - sell['money']}
+        self.totalData = (buy, sell, total)
         win32gui.InvalidateRect(self.hwnd, None, True)
 
     def onClick(self, x, y):
         win32gui.SetFocus(self.hwnd)
-        y -= self.HEAD_HEIGHT
-        if y <= 0:
-            # is in table head 
-            return
-        self.selIdx = y // self.ROW_HEIGHT + self.startIdx
-        win32gui.InvalidateRect(self.hwnd, None, True)
+        if y > self.HEAD_HEIGHT and y < self.getClientSize()[1] - self.TAIL_HEIGHT:
+            y -= self.HEAD_HEIGHT
+            self.selIdx = y // self.ROW_HEIGHT + self.startIdx
+            win32gui.InvalidateRect(self.hwnd, None, True)
 
     def getMaxRowNum(self):
         h = self.getClientSize()[1]
-        h -= self.HEAD_HEIGHT
-        num = h // self.ROW_HEIGHT
+        h -= self.HEAD_HEIGHT + self.TAIL_HEIGHT
+        num = (h + self.ROW_HEIGHT - 1) // self.ROW_HEIGHT
         return num
 
     def onMouseWheel(self, delta):
@@ -466,13 +516,60 @@ class TableWindow(base_win.BaseWindow):
             return True
         return super().winProc(hwnd, msg, wParam, lParam)
 
+class GroupButton(base_win.BaseWindow):
+    def __init__(self, groups, enableGroup = True) -> None:
+        super().__init__()
+        self.groups = groups
+        self.selGroupIdx = -1
+        self.enableGroup = enableGroup
+    
+    # group = int, is group idx
+    def setSelGroup(self, group):
+        if not self.enableGroup:
+            return
+        if type(group) == int:
+            self.selGroupIdx = group
+        win32gui.InvalidateRect(self.hwnd, None, True)
+
+    def onDraw(self, hdc):
+        w, h = self.getClientSize()
+        cw = w / len(self.groups)
+        for i in range(len(self.groups)):
+            item = self.groups[i]
+            color = 0x00008C if self.enableGroup and i == self.selGroupIdx else 0x333333
+            rc = [int(cw * i), 0,  int((i + 1) * cw), h]
+            self.drawer.fillRect(hdc, rc, color)
+            self.drawer.drawRect(hdc, rc, self.drawer.getPen(0x202020))
+            rc[1] = (h - 16) // 2
+            self.drawer.drawText(hdc, item['title'], rc, 0x2fffff)
+
+    def onClick(self, x, y):
+        w, h = self.getClientSize()
+        cw = w / len(self.groups)
+        idx = int(x / cw)
+        if self.enableGroup:
+            self.selGroupIdx = idx
+        self.notifyListener('click', {'group': self.groups[idx], 'groupIdx': idx})
+        win32gui.InvalidateRect(self.hwnd, None, True)
+
+    def winProc(self, hwnd, msg, wParam, lParam):
+        if msg == win32con.WM_LBUTTONUP:
+            x, y = (lParam & 0xffff), (lParam >> 16) & 0xffff
+            self.onClick(x, y)
+            return True
+        return super().winProc(hwnd, msg, wParam, lParam)
+
+
 class FuPanMgrWindow(base_win.BaseWindow):
     def __init__(self) -> None:
         super().__init__()
         self.fsWin = None
         self.tableWin = None
+        self.shareMem = ths_win.ThsShareMemory(False)
+        self.henxi = henxin.HexinUrl()
 
     def create(self):
+        self.shareMem.open()
         style = win32con.WS_OVERLAPPEDWINDOW | win32con.WS_VISIBLE
         rect = (0, 0, 1000, 500)
         super().createWindow(None, rect, style, title = '大单复盘')
@@ -480,19 +577,45 @@ class FuPanMgrWindow(base_win.BaseWindow):
         
         size = self.getClientSize()
         self.tableWin = TableWindow()
-        rc = (0, 50, 250, size[1])
+        rc = (0, 50, 250, size[1] - 50)
         self.tableWin.createWindow(self.hwnd, rc)
 
         rc2 = (rc[2] + 5, 0, size[0] - rc[2] - 10, 400)
         self.fsWin = FenShiWindow()
         self.fsWin.createWindow(self.hwnd, rc2)
-
         self.fsWin.addListener(None, self.tableWin.onListen)
+        
+        grs = (  {'title': '50万', 'val': 50, 'desc': '50万以上'},
+                 {'title': '100万', 'val': 100, 'desc': '100万以上'},
+                 {'title': '300万', 'val': 300, 'desc': '300万以上'},
+                 {'title': '500万', 'val': 500, 'desc': '500万以上'} )
+        self.moneyBtns = GroupButton(grs)
+        rc3 = (63, 10, 190, 30)
+        self.moneyBtns.createWindow(self.hwnd, rc3)
+        self.moneyBtns.addListener(None, self.onListenMoney)
 
-        self.fsWin.update('601096', 20240119)
-        self.fsWin.model.filterDDLR(300)
-        self.tableWin.setData(self.fsWin.model.ddlrFilterData)
-    
+        refreshBtn = GroupButton(({'title': '刷新', 'val': 'Refresh'}, ), False)
+        rc4 = (5, 10, 50, 30)
+        refreshBtn.createWindow(self.hwnd, rc4)
+        refreshBtn.addListener(None, self.onListenRefresh)
+
+    def onListenMoney(self, target, evtName, evtInfo):
+        group = evtInfo['group']
+        ds = self.fsWin.model.filterDDLR(group['val'])
+        self.tableWin.setData(ds, group['desc'])
+
+    def onListenRefresh(self, target, evtName, evtInfo):
+        code = self.shareMem.readCode()
+        day = self.shareMem.readSelDay()
+        if not code or not day:
+            return
+        code = f'{code :06d}'
+        #url = self.henxi.getTodayKLineUrl(code)
+        #data = self.henxi.loadUrlData(url)
+        win32gui.SetWindowText(self.hwnd, f'大单复盘 ({code} / {day})')
+        self.fsWin.update(code, day)
+        self.fsWin.model.filterDDLR(50)
+
 
 if __name__ == '__main__':
     mgr = FuPanMgrWindow()
