e9714c506cbd845f0f43289e343ceec2eef78235
GaoYan <yioo_2007@126.com>
2024-04-03 16:24:12 +0800

    hot

--- THS/hot_win32.py
+++ THS/hot_win32.py
@@ -1,5 +1,5 @@
 import win32gui, win32con , win32api, win32ui # pip install pywin32
-import threading, time, datetime, sys, os
+import threading, time, datetime, sys, os, json, copy
 from multiprocessing import Process
 from multiprocessing.shared_memory import SharedMemory
 
@@ -41,35 +41,60 @@ def showHotWindow():
     win32gui.SetWindowPos(hotWindow.hwnd, 0, x, y, 0, 0, 0x0010|0x0200|0x0001|0x0004)
     hotWindow.rect = (x, y, hotWindow.rect[2], hotWindow.rect[3])
 
-windowsInfo = {}
-def updateWindowInfo(thsWin):
-    _id = id(thsWin)
-    if _id not in windowsInfo:
-        windowsInfo[_id] = {'curPageName': None}
-    wi = windowsInfo[_id]
+class WinStateMgr:
+    def __init__(self, fileName) -> None:
+        self.curPageName = None
+        self.windowsInfo = {}
+        path = os.path.dirname(__file__)
+        path = os.path.join(path, fileName)
+        self.fileName = path
+
+    def read(self):
+        if not os.path.exists(self.fileName):
+            return
+        file = open(self.fileName, 'r')
+        txt = file.read().strip()
+        file.close()
+        if txt:
+            self.windowsInfo = json.loads(txt)
+
+    def save(self):
+        file = open(self.fileName, 'w')
+        txt = json.dumps(self.windowsInfo)
+        file.write(txt)
+        file.close()
+
+def updateWindowInfo(thsWin, stateMgr : WinStateMgr):
+    winsInfo = stateMgr.windowsInfo
     curPageName = thsWin.getPageName()
     if not curPageName:
         return
-    if wi['curPageName'] != curPageName: # changed page
-        wi['curPageName'] = curPageName
-        if curPageName not in wi:
-            wi[curPageName] = {'s1': None, 's2': None, 's3': None}
-        cp = wi[curPageName]
+    if stateMgr.curPageName!= curPageName: # changed page
+        stateMgr.curPageName = curPageName
+        if curPageName not in winsInfo:
+            winsInfo[curPageName] = {'s1': None, 's2': None, 's3': None}
+        cp = winsInfo[curPageName]
         simpleWindow.setWindowState(cp['s1'])
         simpleWindow2.setWindowState(cp['s3'])
         simpleHotZHWindow.setWindowState(cp['s2'])
         if curPageName == '技术分析':
             ths_win.ThsSmallF10Window.adjustPos()
     else:
-        if curPageName not in wi:
-            wi[curPageName] = {'s1': None, 's2': None}
-        cp = wi[curPageName]
-        cp['s1'] = simpleWindow.getWindowState()
-        cp['s2'] = simpleHotZHWindow.getWindowState()
-        cp['s3'] = simpleWindow2.getWindowState()
+        if curPageName not in winsInfo:
+            winsInfo[curPageName] = {'s1': None, 's2': None, 's3': None}
+        cp = winsInfo[curPageName]
+        cp2 = {}
+        cp2['s1'] = simpleWindow.getWindowState()
+        cp2['s2'] = simpleHotZHWindow.getWindowState()
+        cp2['s3'] = simpleWindow2.getWindowState()
+        if cp != cp2:
+            cp.update(cp2)
+            stateMgr.save()
 
-def _workThread(thsWin):
-    global curCode, windowsInfo
+def _workThread(thsWin, fileName):
+    global curCode
+    stateMgr = WinStateMgr(fileName)
+    stateMgr.read()
     while True:
         time.sleep(0.5)
         #mywin.eyeWindow.show()
@@ -81,7 +106,7 @@ def _workThread(thsWin):
         #showHotWindow()
         if win32gui.GetForegroundWindow() != thsWin.topHwnd:
             continue
-        updateWindowInfo(thsWin)
+        updateWindowInfo(thsWin, stateMgr)
         nowCode = thsWin.findCode()
         if curCode != nowCode:
             updateCode(nowCode)
@@ -108,7 +133,7 @@ def subprocess_main():
     simpleWindow2.createWindow(thsWindow.topHwnd)
     simpleHotZHWindow.createWindow(thsWindow.topHwnd)
     hotWindow.addListener(onListen, 'ListenHotWindow')
-    threading.Thread(target = _workThread, args=(thsWindow, )).start()
+    threading.Thread(target = _workThread, args=(thsWindow, 'hot-win32.json')).start()
     win32gui.PumpMessages()
     print('Quit Sub Process')
 
@@ -122,7 +147,7 @@ def subprocess_main_fp():
     simpleWindow.createWindow(thsFPWindow.topHwnd)
     simpleWindow2.createWindow(thsFPWindow.topHwnd)
     simpleHotZHWindow.createWindow(thsFPWindow.topHwnd)
-    threading.Thread(target = _workThread, args=(thsFPWindow, )).start()
+    threading.Thread(target = _workThread, args=(thsFPWindow, 'hot-win32-fp.json')).start()
     win32gui.PumpMessages()
     print('Quit Sub Process(THS FU PING)')    
 
